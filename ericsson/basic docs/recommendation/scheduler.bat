884c286ecd8f12fa7dcaa2c919b1af805ecca6e9
HimaniAttri <47363902+HimaniAttri@users.noreply.github.com>
2021-05-23 23:39:17 +0530

    Add files via upload

---
+++ ericsson/basic docs/recommendation/scheduler.bat
@@ -0,0 +1,4 @@
+
+jupyter nbconvert --execute --inplace Recommendation_Automation_01_20_2020-NBN_AU-R1.ipynb
+
+jupyter nbconvert --execute --inplace Recommendation_Automation_01_20_2020-NBN_AU-R2.ipynb 
